1024389db1211e61e146e3b2b6311456144b5e89
Bochino693 <thiago.bochino2001@gmail.com>
2025-12-13 19:39:50 -0300

    att 13/12

--- lazer/settings.py
+++ lazer/settings.py
@@ -20,12 +20,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    "lazer-sport-1.onrender.com",
+    "lazerandsport.onrender.com",
+    "localhost",
+    "127.0.0.1",
+    "0.0.0.0",
+]
+
 
 # Application definition
 
