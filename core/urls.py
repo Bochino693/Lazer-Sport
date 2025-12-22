985e6a53b2d8b6b08cce6b03f2c0a5fddd125736
Bochino693 <thiago.bochino2001@gmail.com>
2025-12-22 14:15:15 -0300

    att 22/12 pt5

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import (HomeView, BrinquedoInfoView, CategoriasInfoView, BrinquedosView,
                     RegistrarView, LoginUsuarioView, LogoutUsuarioView, EventosView,
                     ProjetosView, ClientePerfilView, ComboInfoView, PromocaoInfoView,
-                    BrinquedoAdmin, NovaCategoria, NovaTag, ComboListView
+                    BrinquedoAdmin, NovaCategoria, NovaTag, ComboView
                     )   # importa views do mesmo app
 from django.conf import settings
 from django.conf.urls.static import static
@@ -26,7 +26,7 @@ urlpatterns = [
     path('categoria/admin/new/', NovaCategoria.as_view(), name='categoria_new'),
     path('tags/admin/new/', NovaTag.as_view(), name='tag_new'),
 
-    path("adm/combos/", ComboListView.as_view(), name="combos_admin"),
+    path("adm/combos/", ComboView.as_view(), name="combos_admin"),
 
 
 
