9612734c6fb3810da44d0d3f1888164313fed53e
Bochino693 <thiago.bochino2001@gmail.com>
2026-01-16 20:56:02 -0300

    att 16/01/26 pt8

--- core/models.py
+++ core/models.py
@@ -93,7 +93,7 @@ class TagsBrinquedos(Prime):
 
 class Estabelecimentos(Prime):
     nome_estabelecimento = models.CharField(max_length=180)
-    imagem_estabelecimento = models.ImageField(upload_to='estabelecimentos/')
+    imagem_estabelecimento = models.ImageField(upload_to='estabelecimentos/', null=True, blank=True)
 
     def __str__(self):
         return self.nome_estabelecimento
