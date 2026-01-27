7ddd4137860582cb109fde20239569a9ac6a2b6f
Bochino693 <thiago.bochino2001@gmail.com>
2026-01-27 10:35:26 -0300

    att 27/01/26 pt1

--- core/models.py
+++ core/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator
-from django.core.exceptions import ValidationError
 from decimal import Decimal, InvalidOperation, ROUND_HALF_UP
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
