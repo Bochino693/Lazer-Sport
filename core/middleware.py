fbab2015836616d64dc5e12ba5c97f0e2e42f805
Bochino693 <thiago.bochino2001@gmail.com>
2026-02-04 15:11:24 -0300

    att 04/02/26 pt

--- core/middleware.py
+++ core/middleware.py
@@ -1,29 +1,27 @@
-from django.conf import settings
-from django.shortcuts import render
-
-from django.http import HttpResponseServerError
-
-
 from django.urls import set_urlconf
+from django.conf import settings
 
 class SubdomainURLMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
+        host = request.get_host().split(':')[0].lower()
         path = request.path
 
+        # 1. Ignorar arquivos estáticos e de sistema para não quebrar o carregamento
         if path.startswith(('/media/', '/static/', '/favicon.ico', '/system/')):
             return self.get_response(request)
 
-        host = request.get_host().split(':')[0]
-
+        # 2. Lógica de Roteamento por Subdomínio
         if host.startswith('interno.'):
             request.is_interno = True
-            set_urlconf('lazer.urls_interno')
+            # Certifique-se de que o arquivo sistema-interno/urls.py existe
+            # Se a pasta tiver hífen, o Python usa underline: sistema_interno.urls
+            set_urlconf('sistema_interno.urls')
         else:
             request.is_interno = False
             set_urlconf('core.urls')
 
-        return self.get_response(request)
-
+        response = self.get_response(request)
+        return response
