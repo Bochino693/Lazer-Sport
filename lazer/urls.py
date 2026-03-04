e666c00556a8acbe46531914400641bf71f54ed8
Lazer & Sport <contato@lazersport.com.br>
2026-03-04 09:15:05 -0300

    04-03-26 pt 3

--- lazer/urls.py
+++ lazer/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from core.views import media_serve
 
 handler404 = 'core.views.erro_404'
 handler500 = 'core.views.erro_500'
@@ -10,7 +9,7 @@ handler500 = 'core.views.erro_500'
 urlpatterns = [
     path('system/', admin.site.urls),
     # MEDIA FORÇADO (produção)
-    path('media/<path:path>/', media_serve),
+    #path('media/<path:path>/', media_serve),
 
     path('accounts/', include('allauth.urls')),
 
