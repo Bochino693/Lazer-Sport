60976a6fd98103e85d62bff57182efd52ffc0514
Lazer & Sport <contato@lazersport.com.br>
2026-03-11 10:55:52 -0300

    att 11-03-26 pt10

--- core/forms.py
+++ core/forms.py
@@ -6,44 +6,21 @@ from django import forms
 from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
-    telefone = forms.CharField(
-        max_length=14,
-        widget=forms.TextInput(attrs={
-            'class': 'form-input',
-            'placeholder': '(00)90000-0000'
-        }),
-        required=False  # opcional, pois será salvo via perfil
-    )
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password']
+        fields = ['first_name', 'last_name', 'email']
         widgets = {
             'first_name': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Nome'}),
             'last_name': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Sobrenome'}),
             'email': forms.EmailInput(attrs={'class': 'form-input', 'placeholder': 'Email'}),
-            'password': forms.PasswordInput(attrs={'class': 'form-input', 'placeholder': 'Senha'}),
         }
+
 class PerfilForm(forms.ModelForm):
+
     class Meta:
         model = ClientePerfil
         fields = ['nome_completo', 'telefone']
-
-        widgets = {
-            'nome_completo': forms.TextInput(
-                attrs={
-                    'class': 'form-input',
-                    'placeholder': 'Nome completo'
-                }
-            ),
-            'telefone': forms.TextInput(
-                attrs={
-                    'class': 'form-input',
-                    'placeholder': '(11)91234-5678',
-                    'maxlength': '15'
-                }
-            )
-                    }
 
 class PromocaoForm(forms.ModelForm):
     class Meta:
