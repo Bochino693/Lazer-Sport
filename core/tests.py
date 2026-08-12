cf2c1f8b15d42d7a5f3b7eff8afc3380ac6d12b5
Thiago Bochino <thiago.bochino2001@gmail.com>
2026-08-12 14:14:44 -0300

    style: colore quantidade e breadcrumbs dos produtos

--- core/tests.py
+++ core/tests.py
@@ -269,6 +269,7 @@ class CatalogImageMigrationTests(TestCase):
         self.assertContains(resposta_brinquedo, "1 / 2")
         self.assertContains(resposta_brinquedo, "produto-breadcrumb ls-breadcrumb")
         self.assertContains(resposta_brinquedo, 'id="bread"')
+        self.assertContains(resposta_brinquedo, "#ffe36b")
         self.assertContains(resposta_brinquedo, 'href="#produto-informacoes"')
         self.assertContains(
             resposta_brinquedo,
@@ -299,6 +300,7 @@ class CatalogImageMigrationTests(TestCase):
         self.assertContains(resposta_peca, "Código LS-P")
         self.assertContains(resposta_peca, "peca-breadcrumb ls-breadcrumb")
         self.assertContains(resposta_peca, 'id="breadcrumb"')
+        self.assertContains(resposta_peca, "#ffc928")
         self.assertContains(resposta_peca, 'href="#peca-informacoes"')
         self.assertContains(
             resposta_peca,
